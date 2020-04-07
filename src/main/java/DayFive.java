871dd50c13792650264e1d9ddcb25e1be9d9e885
soumya-94 <soumya11207@gmail.com>
2020-04-07 22:43:14 +0530

    dayFive

--- src/main/java/DayFive.java
+++ src/main/java/DayFive.java
@@ -1,70 +1,32 @@
-import java.util.ArrayList;
-
-class DayRage{
-
-    int buy, sell;
-}
-
 public class DayFive {
 
-    public int maxProfit(int price[])
+    public int maxProfit(int prices[])
     {
-        int n = price.length;
-        // Prices must be given for at least two days
-        if (n == 1)
-            return 0;
 
-        int count = 0;
-        int finalProfit = 0;
+        int len = prices.length;
+        if(len==0) return 0;
+        int[] profit = new int[len];
 
-        // solution array
-        ArrayList<DayRage> profitDays = new ArrayList<DayRage>();
+        int min=prices[0];
+        profit[0] = 0;
 
-        // Traverse through given price array
-        int i = 0;
-        while (i < n - 1) {
-            // Find Local Minima. Note that the limit is (n-2) as we are
-            // comparing present element to the next element.
-            while ((i < n - 1) && (price[i + 1] <= price[i]))
-                i++;
+        int max_profit = 0;
 
-            // If we reached the end, break as no further solution possible
-            if (i == n - 1)
-                return 0;
+        for(int i=1; i<len; i++)
+        {
+            min = Math.min(min, prices[i]);
 
-            DayRage range = new DayRage();
-            range.buy = i++;
-            // Store the index of minima
-
-            // Find Local Maxima.  Note that the limit is (n-1) as we are
-            // comparing to previous element
-            while ((i < n) && (price[i] >= price[i - 1]))
-                i++;
-
-            // Store the index of maxima
-            range.sell = i - 1;
-            profitDays.add(range);
-
-            // Increment number of buy/sell
-            count++;
-        }
-
-        // print solution
-        if (count == 0)
-            return 0;
-
-        else {
-            for (int j = 0; j < count; j++)
+            int cur_profit = prices[i] - min;
+            if(cur_profit < profit[i-1])
             {
-                System.out.println("Buy on day: " + profitDays.get(j).buy
-                        + "        "
-                        + "Sell on day : " + profitDays.get(j).sell);
-                finalProfit = finalProfit + (price[profitDays.get(j).sell] - price[profitDays.get(j).buy]);
-                //System.out.println(finalProfit);
+                max_profit += profit[i-1];
+                profit[i] = 0;
+                min = prices[i];
             }
-
-            return finalProfit;
+            else
+                profit[i] = Math.max(cur_profit, profit[i-1]);
         }
+        return max_profit + profit[len-1];
     }
 
     public static void main(String args[])
@@ -72,11 +34,11 @@ public class DayFive {
         DayFive stock = new DayFive();
 
         // stock prices on consecutive days
-        int price[] = { 7,1,5,3,6,4 };
+        int prices[] = { 7,1,5,3,6,4 };
 
         // function call
         //stock.stockBuySell(price, n);
-        System.out.println(stock.maxProfit(price));
+        System.out.println(stock.maxProfit(prices));
 
     }
 
